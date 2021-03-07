c375736847ae3f9b87a0b37fe81fd93a418d4a86
brandonbalbada <44941499+brandonbalbada@users.noreply.github.com>
2021-03-07 15:54:12 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,29 @@
+
+window.addEventListener('load', function () {
+    var main = new Vue({
+        el: '#main',
+        data: {
+          isNotClicked: true,
+          shrink: false
+        },
+        mounted() {
+            // window.scrollTo(0,0);
+            // window.addEventListener("scroll", this.scrollMe);
+        },
+        beforeDestroy() {
+            // window.removeEventListener("scroll", this.scrollMe);
+        },
+        methods: {
+            scrollMe: function(){
+           
+                setTimeout(() => {
+                    this.isNotClicked = false;
+                    this.shrink = true
+                }
+                , 500);
+            
+               
+            }
+        },
+      });
+})
