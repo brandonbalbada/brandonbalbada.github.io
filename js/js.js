06d7068ecd4b0ca892714f0c967bbaf2aa766e83
brandonbalbada <44941499+brandonbalbada@users.noreply.github.com>
2021-03-11 17:27:26 +0800

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -4,25 +4,30 @@ window.addEventListener('load', function () {
         el: '#main',
         data: {
           isNotClicked: true,
-          shrink: false
+          shrink: false,
+          isAtBottom: false
         },
         mounted() {
-            
-            // window.addEventListener("scroll", this.scrollMe);
+            window.addEventListener("scroll", this.scrollMe);
         },
         beforeDestroy() {
             // window.removeEventListener("scroll", this.scrollMe);
         },
         methods: {
-            scrollMe: function(){
+            clickMe: function(){
                 window.scrollTo(0,0);
                 
                 setTimeout(() => {
                     this.isNotClicked = false;
-                    this.shrink = true
+                    this.shrink = true;
+                    this.isAtBottom = false;
                 }
               , 500);
-            
+            },
+            scrollMe: function(){
+                
+                this.isAtBottom = document.querySelector("#projects").getBoundingClientRect().bottom <= window.innerHeight ? true : false;
+              
             }
         },
       });
